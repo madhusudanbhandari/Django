c7d5d86bfaeb9e74ee478f191548177a93fd9095
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-04-30 16:39:23 +0545

    fetching database data, showing in table

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from employee.models import Employee
+from django.http import HttpResponse
 # Create your views here.
+
+def employee_detail(request, pk):
+    # try:
+    #     employee=Employee.objects.get(pk=pk)
+    # except:
+    #     raise Http404
+
+    employee=get_object_or_404(Employee,pk=pk)
+   
+    # return HttpResponse(employee)
+    context={
+        'employee':employee
+    }
+
+    return render(request,'employee_detail.html',context)
