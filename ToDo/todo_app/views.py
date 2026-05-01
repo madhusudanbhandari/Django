49aefca10f5a0106f15ed006e18c2e431570027e
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-01 22:05:11 +0545

    login logout

--- ToDo/todo_app/views.py
+++ ToDo/todo_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import RegisterForm
-
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
 def home(request):
     return render(request,"home.html")
 
@@ -12,17 +15,32 @@ def register_view(request):
         if form.is_valid():
             form.save()
             return redirect('login')
+        else:
+            print(form.errors)
         
     else:
         form=RegisterForm()
-        
+
     return render(request,"register.html",{'form':form})
 
 def login_view(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=authenticate(request,username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'login.html')
+    
     return  render(request,"login.html")
 
+@login_required
 def logout_view(request):
-    return render(request,"logout.html")
+    logout(request)
+    return redirect('login')
 
 def delete_view(request,id):
     return  render(request, "delete.html")
