3a18c34c73297bdd836418ec4abda7a77fee5ae2
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-01 21:34:35 +0545

    register page created

--- ToDo/todo_app/views.py
+++ ToDo/todo_app/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import RegisterForm
 
 # Create your views here.
 def home(request):
     return render(request,"home.html")
 
 def register_view(request):
-    return render(request,"register.html")
+    if request.method=="POST":
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        
+    else:
+        form=RegisterForm()
+        
+    return render(request,"register.html",{'form':form})
 
 def login_view(request):
     return  render(request,"login.html")
