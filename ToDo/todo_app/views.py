a0ed6efb4d9d07d286001e2ec066749046950d10
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-01 19:54:43 +0545

    routes created and rendered

--- ToDo/todo_app/views.py
+++ ToDo/todo_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def register_view(request):
+    return render(request,"register.html")
+
+def login_view(request):
+    return  render(request,"login.html")
+
+def logout_view(request):
+    return render(request,"logout.html")
+
+def delete_view(request,id):
+    return  render(request, "delete.html")
