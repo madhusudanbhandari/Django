b526b0b90aa2bbd22ea6412c5b6a75446230a5be
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-01 11:22:42 +0545

    todo

--- ToDo/todo_main/views.py
+++ ToDo/todo_main/views.py
@@ -3,11 +3,9 @@ from django.shortcuts import render
 from todo.models import Task
 
 def home(request):
-    tasks=Task.objects.filter(is_completed=False)
+    tasks=Task.objects.filter(is_completed=False).order_by('-updated_at')
     context={
         'tasks':tasks,
     }
     return render(request,'home.html',context)
 
-def add_task(request):
-    return HttpResponse('Add Task')
