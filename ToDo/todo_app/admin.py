ae85a5f963a0629f68cb490ab0d6969170925171
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-01 19:23:35 +0545

    task model created

--- ToDo/todo_app/admin.py
+++ ToDo/todo_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+
+# admin.site.register(Task) 
+
+@admin.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display=['task','description','created_at','priority']
+    search_fields=['task']
+    list_filter=['is_completed','priority']
--- ToDo/todo_app/models.py
+++ ToDo/todo_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+ 
+class Task(models.Model):
+    task=models.CharField(max_length=250)
+    description=models.TextField()
+    is_completed=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    due_date=models.DateField()
+    priority=models.CharField(
+        max_length=10,
+        choices=[('Low','Low'),('Medium','Medium'),('High','High')],
+        default='Low'
+    )
+
+
+    def __str__(self):
+        return self.task
