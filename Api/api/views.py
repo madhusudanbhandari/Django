f94dbad71feb1a5a8cb05fd7c76c10781cf4ad90
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-03 10:43:54 +0545

    serialization using GET

--- Api/api/views.py
+++ Api/api/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from students.models import student
-
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(['GET'])
 def studentsView(request):
-    students=student.objects.all()
-    students_list=list(students.values())
-    return JsonResponse(students_list,safe=False)
+    # students=student.objects.all()  ##query to fetch the data
+    # students_list=list(students.values())   #  manual serialization
+    
+
+    ##serialization using serializer
+
+    if request.method=='GET':
+        #get all data from student table
+        students=student.objects.all()
+        serializer=StudentSerializer(students,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    
+
+    
