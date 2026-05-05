ecf4b45e83c677d05af9609459c32d5afab5a77e
Madhusudan Bhandari <madhusudanb636@gmail.com>
2026-05-05 12:08:14 +0545

    nested serializers

--- Api/api/urls.py
+++ Api/api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path,include
 from . import views
 from rest_framework.routers import DefaultRouter
 
-router=DefaultRouter()
-router.register('employees',views.EmployeeViewSet,basename='employee')
+# router=DefaultRouter()
+# router.register('employees',views.EmployeeViewSet,basename='employee')
 
 
 urlpatterns=[
@@ -13,6 +13,9 @@ urlpatterns=[
     # path('employees/',views.Employees.as_view()),
     # path('employees/<int:pk>',views.EmployeeDetail.as_view()),
 
-    path('',include(router.urls))
+    # path('',include(router.urls)),
+    path('blogs/',views.BlogsView.as_view()),
+    path('comments/', views.CommentsView.as_view()),
+
 
 ]
--- Api/api/views.py
+++ Api/api/views.py
@@ -8,7 +8,8 @@ from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
 from rest_framework import mixins,generics ,viewsets
-# Create your views here.
+from blogs.models import Blog,Comment
+from blogs.serializers import BlogSerializer,CommentSerializer
 
 @api_view(['GET','POST'])
 def studentsView(request):
@@ -146,33 +147,52 @@ def studentDetailsView(request,pk):
 
 ##ViewSets
 
-class EmployeeViewSet(viewsets.ViewSet):
-    def list(self,request):
-        queryset=Employee.objects.all()
-        serializer=EmployeeSerializer(queryset,many=True)
-        return Response(serializer.data)
+# class EmployeeViewSet(viewsets.ViewSet):
+#     def list(self,request):
+#         queryset=Employee.objects.all()
+#         serializer=EmployeeSerializer(queryset,many=True)
+#         return Response(serializer.data)
     
-    def create(self,request):
-        serializer=EmployeeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors)
+#     def create(self,request):
+#         serializer=EmployeeSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors)
     
-    def retrieve(self,request,pk=None):
-        employee=get_object_or_404(Employee,pk=pk)
-        serializer=EmployeeSerializer(employee)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#     def retrieve(self,request,pk=None):
+#         employee=get_object_or_404(Employee,pk=pk)
+#         serializer=EmployeeSerializer(employee)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def update(self, request, pk=None):
-        employee=get_object_or_404(Employee,pk=pk)
-        serializer=EmployeeSerializer(employee,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.data)
+#     def update(self, request, pk=None):
+#         employee=get_object_or_404(Employee,pk=pk)
+#         serializer=EmployeeSerializer(employee,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.data)
     
-    def delete(self,request,pk=None):
-        employee=get_object_or_404(Employee,pk=pk)
-        employee.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self,request,pk=None):
+#         employee=get_object_or_404(Employee,pk=pk)
+#         employee.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+# class EmployeeViewSet(viewsets.ModelViewSet):
+#     queryset=Employee.objects.all()
+#     serializer_class=EmployeeSerializer
+
+
+
+##Nested Serializer
+
+class BlogsView(generics.ListCreateAPIView):
+    queryset=Blog.objects.all()
+    serializer_class=BlogSerializer
+
+class CommentsView(generics.ListCreateAPIView):
+    queryset=Comment.objects.all()
+    serializer_class=CommentSerializer
+
